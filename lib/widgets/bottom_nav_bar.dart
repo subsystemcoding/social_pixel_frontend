2d7a639ba40f789c22121cb9ce9a26b7b6a17528
ashab272000 <muhammedashab@gmail.com>
2021-01-05 16:04:10 +0400

    feat(bottom_nav_bar): Added icons Changed Height of the nav bar

--- lib/widgets/bottom_nav_bar.dart
+++ lib/widgets/bottom_nav_bar.dart
@@ -6,20 +6,44 @@ class BottomNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 75,
+      height: 70,
       decoration: BoxDecoration(
           color: Theme.of(context).accentColor,
           borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(25.0), topRight: Radius.circular(25.0))),
+              topLeft: Radius.circular(50.0), topRight: Radius.circular(50.0))),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Icon(Icons.home_outlined, color: Colors.white),
-          Icon(Icons.search_outlined, color: Colors.white),
-          Icon(Icons.add, color: Colors.white),
-          Icon(Icons.map_outlined, color: Colors.white),
-          Icon(Icons.message_outlined, color: Colors.white),
-          Icon(Icons.notifications_outlined, color: Colors.white)
+          Icon(
+            Icons.home_outlined,
+            color: Colors.white,
+            size: 28,
+          ),
+          Icon(
+            Icons.search_outlined,
+            color: Colors.white,
+            size: 28,
+          ),
+          Icon(
+            Icons.add,
+            color: Colors.white,
+            size: 28,
+          ),
+          Icon(
+            Icons.map_outlined,
+            color: Colors.white,
+            size: 28,
+          ),
+          Icon(
+            Icons.message_outlined,
+            color: Colors.white,
+            size: 28,
+          ),
+          Icon(
+            Icons.notifications_outlined,
+            color: Colors.white,
+            size: 28,
+          )
         ],
       ),
     );
