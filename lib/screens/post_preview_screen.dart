bbd4afa531bf4ab1cbfe512b0bd595f6513c1bf7
ashab272000 <muhammedashab@gmail.com>
2021-01-27 08:11:56 +0400

    feat(post_preview): Gets time and location
    Decodes image and get information in the initstate
    Screen uses `CircularProgressIndicator` when decoding image
    Add `getInfo()` that is called in `initState` to get time and location
    `_filterImage()` uses the image decoded in the initState

--- lib/screens/post_preview_screen.dart
+++ lib/screens/post_preview_screen.dart
@@ -7,6 +7,7 @@ import 'package:image_cropper/image_cropper.dart';
 import 'package:photofilters/photofilters.dart';
 import 'package:path/path.dart';
 import 'package:image/image.dart' as imageLib;
+import 'package:socialpixel/data/Converter.dart';
 import 'package:socialpixel/widgets/raised_container.dart';
 
 class PostPreviewScreen extends StatefulWidget {
@@ -19,11 +20,19 @@ class PostPreviewScreen extends StatefulWidget {
 
 class _PostPreviewScreenState extends State<PostPreviewScreen> {
   File imageFile;
+  bool gotInfo;
+  imageLib.Image image;
 
   @override
   void initState() {
-    imageFile = File(widget.path);
     super.initState();
+    this.imageFile = File(this.widget.path);
+    this.imageFile.readAsBytes().then((bytes) {
+      image = imageLib.decodeImage(bytes);
+      getInfo();
+    });
+    gotInfo = false;
+    imageFile = File(widget.path);
   }
 
   @override
@@ -32,82 +41,117 @@ class _PostPreviewScreenState extends State<PostPreviewScreen> {
       appBar: AppBar(title: Text('Upload Post')),
       // The image is stored as a file on the device. Use the `Image.file`
       // constructor with the given path to display the image.
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.width,
-            child: Image.file(
-              imageFile,
-              fit: BoxFit.cover,
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
+      body: !gotInfo
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 Container(
-                  height: 150,
-                  child: ListView(
-                    shrinkWrap: true,
-                    padding: EdgeInsets.symmetric(horizontal: 20.0),
-                    scrollDirection: Axis.horizontal,
+                  height: MediaQuery.of(context).size.width,
+                  child: Image.file(
+                    imageFile,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      buildIcon(
-                        context,
-                        text: 'Crop',
-                        iconData: Icons.crop,
-                        onTap: () {
-                          _cropImage(context);
-                        },
+                      Container(
+                        height: 150,
+                        child: ListView(
+                          shrinkWrap: true,
+                          padding: EdgeInsets.symmetric(horizontal: 20.0),
+                          scrollDirection: Axis.horizontal,
+                          children: [
+                            buildIcon(
+                              context,
+                              text: 'Crop',
+                              iconData: Icons.crop,
+                              onTap: () {
+                                _cropImage(context);
+                              },
+                            ),
+                            buildIcon(
+                              context,
+                              text: 'Filter',
+                              iconData: Icons.filter,
+                              onTap: () {
+                                _filterImage(context);
+                              },
+                            ),
+                          ],
+                        ),
                       ),
-                      buildIcon(
-                        context,
-                        text: 'Filter',
-                        iconData: Icons.filter,
-                        onTap: () {
-                          _filterImage(context);
-                        },
+                      SizedBox(
+                        height: 4.0,
+                      ),
+                      Container(
+                        width: 150,
+                        child: ElevatedButton(
+                          onPressed: () => onPressedNext(context),
+                          style: ElevatedButton.styleFrom(
+                            primary: gotInfo
+                                ? Theme.of(context).accentColor
+                                : Theme.of(context)
+                                    .accentColor
+                                    .withOpacity(0.3),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                            ),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Next",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                        ),
                       ),
                     ],
                   ),
                 ),
-                SizedBox(
-                  height: 4.0,
-                ),
-                Container(
-                  width: 150,
-                  child: ElevatedButton(
-                    onPressed: () => onPressedNext(context),
-                    style: ElevatedButton.styleFrom(
-                      primary: Theme.of(context).accentColor,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                      ),
-                    ),
-                    child: Center(
-                      child: Text(
-                        "Next",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.w600),
-                      ),
-                    ),
-                  ),
-                ),
               ],
             ),
-          ),
-        ],
-      ),
     );
   }
 
-  void onPressedNext(BuildContext context) async {
-    var image = imageLib.decodeImage(await this.imageFile.readAsBytes());
+  void getInfo() async {
+    final data = await readExifFromBytes(imageLib.encodeJpg(this.image));
+    DateTime date;
+    String location;
+    if (data.containsKey('Image DateTime')) {
+      date = Converter.exifToDate(data);
+      print(date.toString());
+    }
+    if (data.containsKey('GPS GPSLatitude')) {
+      var lat = data['GPS GPSLatitude']
+          .toString()
+          .replaceAll('[', '')
+          .replaceAll(']', '')
+          .replaceAll(',', '');
+      var long = data['GPS GPSLongitude']
+          .toString()
+          .replaceAll('[', '')
+          .replaceAll(']', '')
+          .replaceAll(',', '');
+      location = '$lat $long';
 
+      print(location);
+    }
+
+    setState(() {
+      gotInfo = true;
+    });
+  }
+
+  void onPressedNext(BuildContext context) async {
     if (image.height > image.width) {
       print("height > width");
       image = imageLib.copyResizeCropSquare(image, image.width);
@@ -190,7 +234,7 @@ class _PostPreviewScreenState extends State<PostPreviewScreen> {
         builder: (context) => new PhotoFilterSelector(
           title: Text("Photo Filter"),
           appBarColor: Theme.of(context).accentColor,
-          image: imageLib.decodeImage(imageFile.readAsBytesSync()),
+          image: image,
           filters: presetFiltersList,
           filename: basename(imageFile.path),
           loader: Center(child: CircularProgressIndicator()),
