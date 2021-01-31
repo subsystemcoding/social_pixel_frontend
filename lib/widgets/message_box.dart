d9b67f05d1f905679ed4ebb607a336bea259db83
ashab272000 <muhammedashab@gmail.com>
2021-01-31 11:36:23 +0400

    feat(message_box) added photo for message box

--- lib/widgets/message_box.dart
+++ lib/widgets/message_box.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
+enum MessageType {
+  Text,
+  Photo,
+  SharedPost,
+}
+
 class MessageBox extends StatelessWidget {
   final bool isUser;
+  final MessageType type;
   final String text;
 
-  const MessageBox({Key key, this.isUser, this.text}) : super(key: key);
+  const MessageBox({
+    Key key,
+    this.isUser,
+    this.text,
+    this.type,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -29,17 +41,45 @@ class MessageBox extends StatelessWidget {
                       bottomLeft: Radius.circular(25.0),
                       bottomRight: Radius.circular(25.0)),
                   color: Theme.of(context).primaryColor),
-          padding: EdgeInsets.all(12.0),
+          padding: EdgeInsets.all(8.0),
           margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
-          child: Text(
-            this.text,
-            style: isUser
-                ? TextStyle(color: Colors.white, fontSize: 20.0)
-                : TextStyle(
-                    color: Theme.of(context).accentColor, fontSize: 20.0),
-          ),
+          child: _buildMessage(context),
         ),
       ],
+    );
+  }
+
+  Widget _buildMessage(BuildContext context) {
+    switch (this.type) {
+      case MessageType.Text:
+        return _buildTextMessage(context);
+      case MessageType.Photo:
+        return _buildPhotoMessage(context);
+      default:
+        return Container();
+    }
+  }
+
+  Widget _buildTextMessage(BuildContext context) {
+    return Text(
+      this.text,
+      style: isUser
+          ? TextStyle(color: Colors.white, fontSize: 12.0)
+          : TextStyle(color: Theme.of(context).accentColor, fontSize: 12.0),
+    );
+  }
+
+  Widget _buildPhotoMessage(BuildContext context) {
+    return Container(
+      width: 150,
+      height: 150,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(25.0),
+        child: Image(
+          image: NetworkImage(this.text),
+          fit: BoxFit.cover,
+        ),
+      ),
     );
   }
 }
