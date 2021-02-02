391449169beba6e06dc71321aee28591c1d43213
ashab272000 <muhammedashab@gmail.com>
2021-02-02 16:39:18 +0400

    feat(home): documented home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,12 +9,17 @@ import 'package:socialpixel/widgets/post_widget.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //initialize a list post
+    // this is a temporary caching method
+    // posts will not disapper if we navigate back to home screen
     List<Post> posts = [];
     final postBloc = BlocProvider.of<PostBloc>(context);
+    // Emitting the fetch event
     postBloc.add(FetchInitialPost());
     return Scaffold(
       appBar: MenuBar().appbar,
       bottomNavigationBar: BottomNavBar(
+        //specifying the currentroute
         currentRoute: '/home',
       ),
       body: Container(
@@ -23,12 +28,14 @@ class HomeScreen extends StatelessWidget {
           children: [
             MenuBar().titleBar(context, title: "Home"),
             Expanded(
+              //Fetch new post when refreshing
               child: RefreshIndicator(
                 onRefresh: () {
                   return Future(() {
                     postBloc.add(FetchNewPost());
                   });
                 },
+                //Listener for errors
                 child: BlocListener<PostBloc, PostState>(
                   listener: (context, state) {
                     if (state is PostError)
@@ -36,11 +43,14 @@ class HomeScreen extends StatelessWidget {
                           content: Text(
                               "Posts are not loaded, please check your internet connection")));
                   },
+                  //builds a list of post after receiving posts from the bloc
                   child: BlocBuilder<PostBloc, PostState>(
                     builder: (context, state) {
                       if (state is PostLoaded) {
+                        //update the posts
                         posts = state.posts;
                       }
+                      //build the posts
                       return buildPosts(context, posts);
                     },
                   ),
@@ -54,10 +64,15 @@ class HomeScreen extends StatelessWidget {
   }
 
   Widget buildPosts(BuildContext context, List<Post> posts) {
+    ///uses a listview builder
+    ///this builds the list lazily
+    ///therefore, not cpu intensive
     return ListView.builder(
       itemCount: posts.length,
       itemBuilder: (BuildContext context, int i) {
         var post = posts[i];
+
+        /// returns a post widget with the available info
         return PostWidget(
           userName: post.userName,
           userAvatar: post.userAvatarLink,
