e90c99518f6661dfaabb0d6d3fff57831971b515
riyakoshy77 <56049161+riyakoshy77@users.noreply.github.com>
2021-03-16 00:38:57 +0400

    edited headings and tried using app bar

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -6,100 +6,74 @@ class SettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var appBar2 = AppBar(title: const Text('Settings'), actions: <Widget>[
+      IconButton(
+        icon: const Icon(Icons.navigate_next),
+        onPressed: () {},
+      ),
+    ]);
     return Scaffold(
+        appBar: appBar2,
         body: Container(
-      margin: EdgeInsets.symmetric(horizontal: 24.0),
-      child: ListView(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+          margin: EdgeInsets.symmetric(horizontal: 24.0),
+          child: ListView(
             children: [
-              SizedBox(
-                height: 48.0,
-              ),
-              Row(
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Icon(
-                    Icons.arrow_back,
-                    color: Colors.black,
-                    size: 40.0,
+                  SizedBox(
+                    height: 48.0,
                   ),
                   SizedBox(
-                    width: 12.0,
+                    height: 24.0,
                   ),
-                  Text(
-                    "Settings",
-                    textAlign: TextAlign.left,
-                    style: Theme.of(context).primaryTextTheme.headline2,
+                  _buildHeading(context, "NOTIFICATIONS"),
+                  SizedBox(
+                    height: 12.0,
                   ),
+                  _buildToggleRow(context, "Pause Notifications"),
+                  _buildToggleRow(context, "Post Upvotes"),
+                  _buildToggleRow(context, "Post Comments"),
+                  _buildToggleRow(context, "Post Shares"),
+                  _buildToggleRow(context, "Post Mentions"),
+                  _buildToggleRow(context, "Comment Mentions"),
+                  _buildToggleRow(context, "Comment Replies"),
+                  SizedBox(
+                    height: 24.0,
+                  ),
+                  _buildHeading(context, "PRIVACY"),
+                  SizedBox(
+                    height: 12.0,
+                  ),
+                  _buildRow(context, "Blocked Accounts"),
+                  _buildRow(context, "Muted Accounts"),
+                  SizedBox(
+                    height: 24.0,
+                  ),
+                  _buildHeading(context, "SECURITY"),
+                  SizedBox(
+                    height: 12.0,
+                  ),
+                  _buildRow(context, "Change Password"),
+                  _buildRow(context, "Login Activity"),
+                  SizedBox(
+                    height: 24.0,
+                  ),
+                  _buildHeading(context, "ACCOUNT"),
+                  SizedBox(
+                    height: 12.0,
+                  ),
+                  _buildRow(context, "Personal Information"),
+                  _buildRow(context, "Language"),
+                  _buildToggleRow(context, "Mobile Data Sver"),
+                  _buildToggleRow(context, "Save To CameraRoll"),
+                  _buildRow(context, "Request Verification"),
+                  _buildRow(context, "Share to Other Applications"),
                 ],
               ),
-              SizedBox(
-                height: 24.0,
-              ),
-              Text(
-                "NOTIFICATIONS",
-                textAlign: TextAlign.left,
-                style: Theme.of(context).primaryTextTheme.headline3,
-              ),
-              SizedBox(
-                height: 12.0,
-              ),
-              _buildToggleRow(context, "Pause Notifications"),
-              _buildToggleRow(context, "Post Upvotes"),
-              _buildToggleRow(context, "Post Comments"),
-              _buildToggleRow(context, "Post Shares"),
-              _buildToggleRow(context, "Post Mentions"),
-              _buildToggleRow(context, "Comment Mentions"),
-              _buildToggleRow(context, "Comment Replies"),
-              SizedBox(
-                height: 24.0,
-              ),
-              Text(
-                "PRIVACY",
-                textAlign: TextAlign.left,
-                style: Theme.of(context).primaryTextTheme.headline3,
-              ),
-              SizedBox(
-                height: 12.0,
-              ),
-              _buildRow(context, "Blocked Accounts"),
-              _buildRow(context, "Muted Accounts"),
-              SizedBox(
-                height: 24.0,
-              ),
-              Text(
-                "SECURITY",
-                textAlign: TextAlign.left,
-                style: Theme.of(context).primaryTextTheme.headline3,
-              ),
-              SizedBox(
-                height: 12.0,
-              ),
-              _buildRow(context, "Change Password"),
-              _buildRow(context, "Login Activity"),
-              SizedBox(
-                height: 24.0,
-              ),
-              Text(
-                "ACCOUNT",
-                textAlign: TextAlign.left,
-                style: Theme.of(context).primaryTextTheme.headline3,
-              ),
-              SizedBox(
-                height: 12.0,
-              ),
-              _buildRow(context, "Personal Information"),
-              _buildRow(context, "Language"),
-              _buildToggleRow(context, "Mobile Data Sver"),
-              _buildToggleRow(context, "Save To CameraRoll"),
-              _buildRow(context, "Request Verification"),
-              _buildRow(context, "Share to Other Applications"),
             ],
           ),
-        ],
-      ),
-    ));
+        ));
   }
 
   Widget _buildRow(BuildContext context, String text) {
@@ -128,6 +102,14 @@ class SettingsScreen extends StatelessWidget {
           size: 50.0,
         )),
       ],
+    );
+  }
+
+  Widget _buildHeading(BuildContext context, String text) {
+    return Text(
+      text,
+      textAlign: TextAlign.left,
+      style: Theme.of(context).primaryTextTheme.headline3,
     );
   }
 }
